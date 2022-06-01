8a3ed0e905175ee22f145b04a24079c178b6bed8
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-06-01 11:03:38 +0200

    Modified preload.js to fit with new main.js

--- preload.js
+++ preload.js
@@ -5,3 +5,10 @@ contextBridge.exposeInMainWorld("api", {
   // Note that the os.cpus() is a Node.js method calling the OS
   cpuCount: () => os.cpus().length,
 });
+async function handleSaveFile(todos) {
+  return ipcRenderer.invoke("saveFile", todos);
+}
+contextBridge.exposeInMainWorld("api", {
+  cpuCount: () => os.cpus().length,
+  saveFile: handleSaveFile,
+});
