7eccf8b06bf949bcca8d7dcccafd565b89b109a4
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-06-01 10:00:35 +0200

    Added styling for router links in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,22 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
+  <div class="container-fluid">
+    <div class="row">
+      <div
+        class="col-2 vh-100 d-flex flex-column align-items-center bg- gray-200 divider-right"
+      >
+        <RouterLink to="/" class="btn btn-secondary w-75 my- 2"
+          >Home</RouterLink
+        >
 
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <RouterLink to="/about" class="btn btn-secondary w-75 my- 2"
+          >About</RouterLink
+        >
+      </div>
+      <div class="col-10">
+        <RouterView />
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
