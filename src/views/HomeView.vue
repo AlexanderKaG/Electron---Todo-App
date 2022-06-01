d63fc93b18140e19a7532578f1b2372040b4aa96
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-06-01 10:05:04 +0200

    Added todo in homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup>
-import ElectronApi from "@/components/ElectronApi.vue";
+<script>
+import { mapStores } from "pinia";
+import { useTodoStore } from "@/stores/todo.js";
+export default {
+  name: "HomeView",
+  computed: {
+    ...mapStores(useTodoStore),
+  },
+};
 </script>
 
 <template>
-  <main>
-    <ElectronApi />
-  </main>
+  <div v-for="todo in todoStore.items" :key="todo.title" class="card my-2">
+    <div class="card-header">
+      <h3 class="d-inline">
+        <time>{{ todo.date }}</time> {{ todo.title }}
+      </h3>
+    </div>
+    <div class="card-body d-flex justify-content-between">
+      <div>{{ todo.description }}</div>
+      <div class="min-width-max-content">
+        <button class="btn btn-sm btn-primary mx-2">Done</button>
+        <button class="btn btn-sm btn-secondary">Remove</button>
+      </div>
+    </div>
+  </div>
 </template>
